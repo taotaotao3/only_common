b19f18a2a98602018d667d6c8b97eeaa42eba860
Dixon Takao <tmolita077@gmail.com>
2021-08-08 04:25:08 +0900

    For duplicate cases

--- only_common.py
+++ only_common.py
@@ -19,35 +19,30 @@ def excommon(arg_1 = 'a.csv', arg_2 = 'b.csv', arg_3 = 'shift-jis'):
     list_b = list(df_b.loc[0][0])
 
     after_content = ""
-    def duplicate_delete_csv(content, content2, after_content):
+    after_content2 = ""
+    flag_last = "0"
+    def duplicate_delete_csv(content, content2, after_content, after_content2, flag_last):
+        after_content = content
+        after_content2 = content2
         for i in range(len(content)):
+            if i > int(len(content2)-1):
+                after_content = content[:i]
+                flag_last = "1"
+                return after_content, after_content2, flag_last
+            if len(content) - 1 == i:
+                flag_last = "1"
             if content[i] != content2[i]:
                 for num in range(len(content) - i):
                     if content2[i] == content[i+num]:
                         after_content = content[:i] + content[(i+num):]
-                        return after_content
-        return content
-
+                        return after_content, after_content2, flag_last
+                after_content2 = content2[:i] + content2[i+1:]
+                return after_content, after_content2, flag_last
 
     while list_a != list_b:
-        if(len(list_a) > len(list_b)):
-            list_a_old = list_a
-            list_a = duplicate_delete_csv(list_a, list_b, after_content)
-            if list_a_old == list_a:
-                break
-        elif(len(list_a) < len(list_b)):
-            list_b_old = list_b
-            list_b = duplicate_delete_csv(list_b, list_a, after_content)
-            if list_b_old == list_b:
-                break
-        else:
-            list_a_old = list_a
-            list_a = duplicate_delete_csv(list_a, list_b, after_content) 
-            list_b_old = list_b
-            list_b = duplicate_delete_csv(list_b, list_a, after_content)
-            if list_b_old == list_b:
-                break
-
+        list_a, list_b, flag_last = duplicate_delete_csv(list_a, list_b, after_content, after_content2, flag_last) 
+        if flag_last == "1":
+            break
     StrA = "".join(list_a)
     print('Only common parts:', StrA)
     sys.exit
